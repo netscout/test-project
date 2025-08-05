f13481f725e76e9950aa8f4b3a25f2d3688b46cd
OnLifeCoding <netscout82@naver.com>
2025-08-05 13:37:15 +0900

    feat: 아주 무시무시한 문제가 있는 함수

---
+++ src/utils/some-bad-utils.ts
@@ -0,0 +1,27 @@
+/**
+ * 아주 무시무시한 문제가 있는 함수
+ */
+export const someBadUtils = () => {
+  let a = 1;
+  let b = 2;
+  let c = 3;
+  let d = 4;
+  let e = 5;
+  let f = 6;
+  let g = 7;
+  let h = 8;
+  let i = 9;
+  let j = 10;
+
+  if (a > b) {
+    if (c > d) {
+      if (e > f) {
+        if (g > h) {
+          if (i > j) {
+            return true;
+          }
+        }
+      }
+    }
+  }
+};
